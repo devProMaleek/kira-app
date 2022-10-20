b51d1bd1ffed9ee435555509bedd9f97a9be38c2
devProMaleek <milikiadbay@gmail.com>
2022-10-20 14:21:25 +0100

    feat:Add github worflow file and deploy.sh

---
+++ .scripts/deploy.sh
@@ -0,0 +1,32 @@
+
+#!/bin/bash
+set -e
+
+echo "Deployment started ..."
+
+# Enter maintenance mode or return true
+# if already is in maintenance mode
+(php artisan down --message "The app is beeing (quickly) updated. Please try again in a few minutes") || true
+
+# Pull the latest version of the app
+git pull origin production
+
+# Install composer dependencies
+composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
+
+# Clear the old cache
+php artisan clear-compiled
+
+# Recreate cache
+php artisan optimize
+
+# Compile npm assets
+npm run prod
+
+# Run database migrations
+php artisan migrate --force
+
+# Exit maintenance mode
+php artisan up
+
+echo "Application deployed successfully"
